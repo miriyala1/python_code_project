f8472c0f58a070b3327c8e0ba27396d0318cdbfb
miriyala1 <miriyaladivya07@gmail.com>
2024-09-24 01:53:26 -0400

    Delete first.py

--- first.py
+++
@@ -1,9 +0,0 @@
-######################################
-# This code comes from: https://github.com/crewAIInc/crewAI-examples/blob/main/stock_analysis/main.py 
-# And is licensed under MIT
-######################################
-#
-# The following keys must be defined in the environment shell
-# OPENAI_API_KEY=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
-# SEC_API_API_KEY=edd2540a66b00d4fa9a0c7f05077bda5a001e01283c6bcc4568ac05e0239ad6a
-# SERPER_API_KEY =82d61422f23a5c4b0f961a56802d19b81e1b30bd
